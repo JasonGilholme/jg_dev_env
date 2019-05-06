9676649886ee87d2d99ca577fca6e78ab9147d3a
Jason Gilholme <jasongilholme@gmail.com>
2019-05-06 19:48:54 +1000

    Zip content for release packages

--- utils/install_app.sh
+++ utils/install_app.sh
@@ -2,26 +2,36 @@
 
 SOURCE_DIR=$1
 INSTALL_DIR=$2
+APP_NAME=$3
 
 echo Installing App: 
 echo ----Source Dir: $SOURCE_DIR
 echo ----Install Dir: $INSTALL_DIR
 
-mkdir -p $INSTALL_DIR/lib/
+mkdir -p $INSTALL_DIR/apps/$APP_NAME/lib/
 
-find $INSTALL_DIR -executable -type f | xargs -I '{}' ldd '{}' | grep '=> /' | awk '{print $3}' | xargs -I '{}' cp -v '{}' $INSTALL_DIR/lib
+find $INSTALL_DIR/apps/$APP_NAME -executable -type f | xargs -I '{}' ldd '{}' | grep '=> /' | awk '{print $3}' | xargs -I '{}' cp -v '{}' $INSTALL_DIR/apps/$APP_NAME/lib
 
 INSTALL_SCRIPT=$SOURCE_DIR/.install
 if [ -f $INSTALL_SCRIPT ]; then
-    cp -v $INSTALL_SCRIPT $INSTALL_DIR/.install
+    cp -v $INSTALL_SCRIPT $INSTALL_DIR/apps/$APP_NAME/.install
 fi
 
 BASH_PROF=$SOURCE_DIR/.bash_profile
 if [ -f $BASH_PROF ]; then
-    cp -v $BASH_PROF $INSTALL_DIR/.bash_profile
+    cp -v $BASH_PROF $INSTALL_DIR/apps/$APP_NAME/.bash_profile
 fi
 
 BASH_RC=$SOURCE_DIR/.bashrc
 if [ -f $BASH_RC ]; then
-    cp -v $BASH_RC $INSTALL_DIR/.bashrc
+    cp -v $BASH_RC $INSTALL_DIR/apps/$APP_NAME/.bashrc
 fi
+
+cd $INSTALL_DIR
+mkdir -p _archives
+
+pwd
+ls -la
+zip -yr _archives/$APP_NAME.zip apps/$APP_NAME
+
+
